e1c202a756cc269630e1f5916be961259e7b1d89
Kacper Szafrański <kacperr.szafranski@gmail.com>
2025-09-23 21:55:21 +0200

    setup project

---
+++ settings.gradle.kts
@@ -0,0 +1,19 @@
+rootProject.name = "cityxplore"
+
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+include(":backend")
+include(":client:composeApp")
